6bc80785e5062255b9cfb84307f665603105d021
aldibergjr <agcj2@cin.ufpe.br>
2022-04-27 16:25:55 -0300

    Updated SAM with new data collector version and script fixes

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-if [[ $# -eq 0 ]] ; then
+if [ $# -eq 0 ] ; then
     echo 'No directory passed as parameter'
     exit 1
 fi
@@ -6,7 +6,7 @@ repo=$1/.git/hooks
 echo "instaling on repository:" $repo
 if tar -xzvf tool.tar.gz --directory $repo --strip-components 1; then
     cd $repo/dynamic-analysis-dependencies
-    git clone https://github.com/leusonmario/SMAT.git
+    git clone --branch coverage-metric https://github.com/leusonmario/SMAT.git
     echo "Finished instalation."
 else
     echo "Not a git repository"
